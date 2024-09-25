234f6570fe5c90832a70c253a3363a079874795f
RoxasTA <victorbr64g@gmail.com>
2024-09-25 17:32:10 -0300

    Api service call

--- Sources/App/Service/ApiService.swift
+++ Sources/App/Service/ApiService.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import Combine
+//import Combine
 import Vapor
 
 class ApiService: @unchecked Sendable {
@@ -17,21 +17,44 @@ class ApiService: @unchecked Sendable {
     
     private init() { }
     
-    public func fetchData<T: Codable>(url: URL, object: Data? = nil, httpMethod: HttpMethods) throws -> AnyPublisher<T, Error> {
+    public func fetchData<T: Decodable>(url: URL, object: Data? = nil, httpMethod: HTTPMethod, completion: @escaping (Result<T, URLError>) -> Void) {
+        
         var urlRequest = URLRequest(url: url)
         urlRequest.httpMethod = httpMethod.rawValue
-        urlRequest.httpBody = object
-        urlRequest.setValue(MIME.jsonAp.rawValue, forHTTPHeaderField: MIME.jsonType.rawValue)
+        urlRequest.httpBody = object.map(\.self)
+        urlRequest.setValue(MIME.jsonAp.rawValue, forHTTPHeaderField: MIME.jsonAp.rawValue)
         
-        return urlSession.dataTaskPublisher(for: urlRequest)
-            .tryMap { [weak self] output in
-                try self?.checkResponse(output.response)
-                return output.data
+        let task = urlSession.dataTask(with: urlRequest) { [weak self] data, response, error in
+            
+            if let error = error {
+                completion(.failure(error as! URLError))
+                return
             }
-            .decode(type: T.self, decoder: JSONDecoder())
-            .receive(on: DispatchQueue.main)
-            .eraseToAnyPublisher()
+            
+            do {
+                try self?.checkResponse(response!)
+                
+                guard let data else {
+                    completion(.failure(URLError(.badServerResponse)))
+                    return
+                }
+                
+                let decodedObject = try JSONDecoder().decode(T.self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(decodedObject))
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error as! URLError))
+                }
+            }
+        }
+        task.resume()
+        
+        
     }
+    
+    
     
     /// Verifica a resposta da chamada à API.
     private func checkResponse(_ response: URLResponse) throws {
