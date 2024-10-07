07ab3c625ffdd3287448c0897bea8dcfac55d648
GustavoHoreste <101297032+GustavoHoreste@users.noreply.github.com>
2024-10-07 13:59:24 -0300

    configured heroku database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,15 +7,26 @@ import Vapor
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    
+    if let databaseURL = Environment.get("postgres://u2vs4oe2e5c9ia:p6d7611b3616aa719bd7fed94226c898abdb01883624a1a6240cf634eeeb13966@c9pv5s2sq0i76o.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d30efhlso9ece0") {
+        var tlsConfig: TLSConfiguration = .makeClientConfiguration()
+        tlsConfig.certificateVerification = .none
+        let nioSSLContext = try NIOSSLContext(configuration: tlsConfig)
 
-    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-        username: Environment.get("admin_username") ?? "vapor_username",
-        password: Environment.get("12345678") ?? "vapor_password",
-        database: Environment.get("macro_database") ?? "vapor_database",
-        tls: .prefer(try .init(configuration: .clientDefault)))
-    ), as: .psql)
+        var postgresConfig = try SQLPostgresConfiguration(url: databaseURL)
+        postgresConfig.coreConfiguration.tls = .require(nioSSLContext)
+
+        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+    } else {
+        app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
+            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+            port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
+            username: Environment.get("admin_username") ?? "vapor_username",
+            password: Environment.get("12345678") ?? "vapor_password",
+            database: Environment.get("macro_database") ?? "vapor_database",
+            tls: .prefer(try .init(configuration: .clientDefault)))
+        ), as: .psql)
+    }
 
     app.migrations.add(OrderMigration())
     app.migrations.add(ProductMigration())
@@ -23,7 +34,9 @@ public func configure(_ app: Application) async throws {
     
     app.http.client.configuration.redirectConfiguration = .disallow
     
-    try await app.autoMigrate()
+    if app.environment = .development {
+        try await app.autoMigrate()
+    }
 
     try routes(app)
 }
